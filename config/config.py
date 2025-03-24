5d707c3312af2d2dda1c528b71f445cfe5c7a452
TopSGW <web.smartdev22@gmail.com>
2025-03-24 05:43:05 -0700

    change ollama url

--- config/config.py
+++ config/config.py
@@ -14,5 +14,5 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-OLLAMA_URL = "http://157.157.221.29:20625"
+OLLAMA_URL = "http://157.157.221.29:20786"
 os.environ['OLLAMA_HOST'] = OLLAMA_URL
